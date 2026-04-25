53b845bb4abead4eed562c49e4dcf50e5a64f568
enzoavila2026-ops <enzoavila2026@gmail.com>
2026-04-25 04:05:32 -0300

    Update Build.gradle.kts

--- Build.gradle.kts
+++ Build.gradle.kts
@@ -1,5 +1,16 @@
-// Top-level build file where you can add configuration options common to all sub-projects/modules.
-plugins {
-    id("com.android.application") version "8.2.2" apply false
-    id("org.jetbrains.kotlin.android") version "1.9.23" apply false
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
 }
+dependencyResolution {
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "AI4KEnhancer"
+include(":app")
